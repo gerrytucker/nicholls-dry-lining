254cde23a12eccfa27ad7ce113280a2e5c3f3e7b
gerrytucker <gerrytucker@gerrytucker.co.uk>
2014-12-18 15:47:04 +0000

    Auto-generated commit 18/12/2014 3:46:58 PM

--- index.php
+++ index.php
@@ -2,8 +2,12 @@
 
 		<div id="services" class="grid-full">
 			<div class="row">
-				<div class="services column6"></div>
-				<div class="services column6"></div>
+				<div class="services column6">
+					<h2 class="fadeInLeft">Plastering</h2>
+				</div>
+				<div class="services column6">
+					<h2 class="fadeInRight">Dry Lining</h2>
+				</div>
 			</div>
 		</div>
 
