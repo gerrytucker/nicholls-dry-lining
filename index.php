c3ebe9eb19eb95beb8c57c23a56064ec666bfec5
gerrytucker <gerrytucker@gerrytucker.co.uk>
2014-12-19 14:43:27 +0000

    Auto-generated commit 19/12/2014 2:43:17 PM

--- index.php
+++ index.php
@@ -11,4 +11,14 @@
 			</div>
 		</div>
 
+		<div id="contact" class="grid-full">
+			<div class="row">
+				<div class="contact column6">
+					<?php echo do_shortcode( '[contact-form-7 id="7" title="Contact Us"]' ); ?>
+				</div>
+				<div class="contact column6">
+				</div>
+			</div>
+		</div>
+
 <?php get_footer(); ?>
