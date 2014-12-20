9c9d3cf0b6d87ee8050274396fd3ad143ccae831
gerrytucker <gerrytucker@gerrytucker.co.uk>
2014-12-20 18:00:24 +0000

    Auto-generated commit 20/12/2014 6:00:16 PM

--- index.php
+++ index.php
@@ -12,13 +12,13 @@
 
 	$pages = get_pages( $args );
 	foreach( $pages as $page ) :
-		$title = get_the_title();
 		if ( has_post_thumbnail( $page->ID ) ) {
-			$thumb_url = wp_get_attachment_image_src( get_post_thumbnail_id( $page->ID ), 'large' );
+			$thumb_url = wp_get_attachment_url( get_post_thumbnail_id( $page->ID ), 'large' );
 		}
 ?>
+				
 				<div class="services column6" style="background: url(<?php echo $thumburl[0]; ?>) no repeat center center">
-					<span><?php echo $title; ?></span>
+					<span><?php echo $page=>post_title; ?></span>
 				</div>
 
 <?php endforeach; ?>				
